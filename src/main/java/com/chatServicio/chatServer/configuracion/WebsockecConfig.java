ac937b2cea40c3ee952ca13b920389615db44792
Kiana <kbermudez@asap-consulting.net>
2023-11-04 22:09:35 -0300

    config webSockect

--- src/main/java/com/chatServicio/chatServer/configuracion/WebsockecConfig.java
+++ src/main/java/com/chatServicio/chatServer/configuracion/WebsockecConfig.java
@@ -1,4 +1,4 @@
-package configuracion;
+package com.chatServicio.chatServer.configuracion;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
